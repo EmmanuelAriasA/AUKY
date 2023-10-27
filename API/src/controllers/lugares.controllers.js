661ef03081b0276b144b616a516ced112791b538
EmmanuelAriasA <mane.ariasa@gmail.com>
2023-10-26 22:03:03 -0600

    endPoint lugaresById

--- API/src/controllers/lugares.controllers.js
+++ API/src/controllers/lugares.controllers.js
@@ -12,13 +12,25 @@ export const getLugares = async (req, res) => {
 
 export const getLugaresByDestacado = async (req, res) => {
   try {
-    const [rows] = await pool.query(
-      "SELECT * FROM lugar WHERE destacado = 1"
-    );
+    const [rows] = await pool.query("SELECT * FROM lugar WHERE destacado = 1");
     if (rows.length === 0) {
       res.json({ message: "No hay lugares destacados" });
     }
 
     res.json(rows);
-  } catch (error) {}
+  } catch (error) {
+    handleErrors(res, error);
+  }
+};
+
+export const getLugaresById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM lugar WHERE id = ?", [id]);
+    if (rows.length === 0) {
+      res.json({ message: "No hay lugares con ese id" });
+    }
+  } catch (error) {
+    handleErrors(res, error);
+  }
 };
