00bce459f0a73b0ffa41d869ca055b34db600e4a
TesRodM <143141275+TesRodM@users.noreply.github.com>
2023-10-29 22:39:04 -0600

    Update DetalleLugar.jsx

--- AUIKY/src/components/DetalleLugar.jsx
+++ AUIKY/src/components/DetalleLugar.jsx
@@ -5,10 +5,10 @@ const DetalleLugar = ({ Data }) => {
   return (
     <div className="especificaciones">
       <h2>{Nombre}</h2>
-      <p><strong>Costos:</strong> {Costos}</p>
-      <p><strong>Descripción:</strong> {Descripcion}</p>
-      <p><strong>Horario:</strong> {Horario}</p>
-      <p><strong>Calificación:</strong> {Calificaciones}</p>
+      <p>{Costos}</p>
+      <p>{Descripcion}</p>
+      <p>{Horario}</p>
+      <p>{Calificaciones}</p>
       <p>{Imagen}</p>
     </div>
   );
