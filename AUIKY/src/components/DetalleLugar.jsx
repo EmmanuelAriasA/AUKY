5a6e4bf65bf9ea601f5f4f3787feedb40ffea1d1
TesRodM <143141275+TesRodM@users.noreply.github.com>
2023-10-29 22:30:02 -0600

    Update DetalleLugar.jsx

--- AUIKY/src/components/DetalleLugar.jsx
+++ AUIKY/src/components/DetalleLugar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const DetalleLugar = ({ Data }) => {
-  const { Nombre, Horario, Calificaciones } = Data;
+  const { Nombre, Costos, Descripcion, Horario, Calificaciones, Imagen} = Data;
   return (
     <div className="especificaciones">
       <h2>{Nombre}</h2>
-      {/* <p><strong>Dirección:</strong> {address}</p> */}
+      <p><strong>Costos:</strong> {Costos}</p>
+      <p><strong>Descripción:</strong> {Descripcion}</p>
       <p><strong>Horario:</strong> {Horario}</p>
       <p><strong>Calificación:</strong> {Calificaciones}</p>
+      <p>{Imagen}</p>
     </div>
   );
 };
