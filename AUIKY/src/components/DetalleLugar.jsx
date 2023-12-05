a65c31b98aea490c38332fc072d03f9183a1fa6e
Jessica Mariana Martínez Coronilla <98509200+MarianaMtz25@users.noreply.github.com>
2023-12-05 15:04:26 -0600

    Mapa añadido
    Se puede visualizar el mapa en detalles del lugar seleccionado

--- AUIKY/src/components/DetalleLugar.jsx
+++ AUIKY/src/components/DetalleLugar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import '../styles/DetalleLugar.css';
 import AUIKY from '../assets/LOGO AUIKY.png';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const DetalleLugar = ({ Data }) => {
-  const { Nombre, Costos, Descripcion, Horario, Calificaciones, Imagen} = Data;
+  const { Nombre, Costos, Descripcion, Horario, Calificaciones, Imagen, Latitud, Longitud } = Data;
+
+  const apiKey = 'AIzaSyCgiG3PUkJi1LXspjPQyBWvxOk18GVcia0';
+  const mapContainerStyle = {
+    width: '100%',
+    height: '300px',
+  };
+
+  const center = {
+    lat: Latitud,
+    lng: Longitud,
+  };
+
+  const mapaUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${Latitud},${Longitud}&zoom=15&size=300x200&maptype=roadmap&markers=color:red%7Clabel:Lugar%7C${Latitud},${Longitud}&key=${apiKey}`;
+
+  //const mapaUrl = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCgiG3PUkJi1LXspjPQyBWvxOk18GVcia0&language=es&q=${Latitud},${Longitud}';
   return (
     <div className="App">
       <div className="grid">
@@ -28,7 +44,13 @@ const DetalleLugar = ({ Data }) => {
           <p>{Calificaciones}</p>
         </div>
         <div className="image" colSpan="5">
-          <img className="place-image" src={Imagen} alt="Imagen del lugar" />
+        <img className="place-image" src={Imagen} alt="Imagen del lugar" />
+        <br></br>
+        <LoadScript googleMapsApiKey={apiKey}>
+            <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={15}>
+              <Marker position={center} />
+            </GoogleMap>
+          </LoadScript>
         </div>
       </div>
       <div className="footer">
